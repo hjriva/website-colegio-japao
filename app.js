a6c06bf157ce5c215e51a33fd2f7c4e955585185
Helena <157857573+hjriva@users.noreply.github.com>
2026-05-07 07:49:24 -0300

    retirando reloader

--- app.js
+++ app.js
@@ -75,19 +75,3 @@ app.post('/Alteracao_BD', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
-
-const url = `https://tcc-alt-ios.onrender.com/`; // Replace with your Render URL
-const interval = 30000; // Interval in milliseconds (30 seconds)
-
-//Reloader Function
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
-
-setInterval(reloadWebsite, interval);
